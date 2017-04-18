a8fd3cf0d78dcd94b7d1d88d3ccb97f51033edd1
Fiorella Cisneros <fiorella.0624@gmail.com>
2017-04-17 20:56:07 -0500

    Eliminar Imagen

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,4 +3,28 @@ window.addEventListener('load',function(event){
 
 	var container = document.getElementById('contenido');
 
+	for (var i = 1; i < 5; i++){
+		var div = document.createElement('div');
+		var span = document.createElement('span');
+		var img = document.createElement('img');
+		var url = "assets/img/" + i + ".jpg";
+
+		img.setAttribute('src', url);
+		div.appendChild(img);
+		span.innerText = "x";
+		div.appendChild(span);
+
+		div.setAttribute('id', 'imagen-' + i );
+		span.setAttribute('data-imagen' , i);
+		div.setAttribute('class','img-box');
+		container.appendChild(div);
+
+		span.addEventListener('click',function(event){
+			var id = 'imagen-' + this.getAttribute('data-imagen');
+			var img_box = document.getElementById(id);
+			img_box.setAttribute('class', 'img-box deleted');
+		});
+	}
+
+
 });
